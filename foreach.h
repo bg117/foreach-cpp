811cc7ff5a07813b39944b2c7dc75b6d88e353fb
bg117 - The OpenCode Team <76200573+bg117@users.noreply.github.com>
2021-06-20 22:11:35 +0800

    Update foreach.h

--- foreach.h
+++ foreach.h
@@ -1,9 +1,10 @@
 #pragma once
-#if __cplusplus >= 201703L
-#include <tuple>
 
 // Foreach loop in C++. The variable's type is deduced from the first element of the collection being iterated. Works only in C++ 17 and up.
-#define foreach(var,collection) for (auto [i, var] = std::tuple{0, collection[0]}; i < sizeof(collection) / sizeof(*collection); i++, var = collection[i])
-#else
-#error foreach is only supported in C++ 17 and up! Will add support for lower versions in the future.
-#endif
+#define foreach(item, array)                         \
+    for (int keep = 1,                               \
+             count = 0,                              \
+             size = sizeof(array) / sizeof (*array); \
+         keep && count != size;                      \
+         keep = !keep, count++)                      \
+         for (item = array[count]; keep; keep = !keep)
